54a891b152cf3e2a696bb299e26465ea681ce024
毛爽然 <2021011784@secoder.net>
2023-04-06 14:48:43 +0800

    relation fixed 3

--- style-test.sh
+++ style-test.sh
@@ -1,2 +1,2 @@
-pycodestyle DjangoHW UserManage utils > pycodestyle-report.txt
-pylint DjangoHW UserManage utils > pylint-report.txt
+pycodestyle DjangoHW UserManage FriendRelation utils > pycodestyle-report.txt
+pylint DjangoHW UserManage FriendRelation utils > pylint-report.txt
