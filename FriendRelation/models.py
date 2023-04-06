8251d64a143f14e6fca1bf6bb2d7a1acd3cba261
毛爽然 <2021011784@secoder.net>
2023-04-06 16:53:53 +0800

    relation inited 2

--- FriendRelation/models.py
+++ FriendRelation/models.py
@@ -4,5 +4,16 @@ from UserManage.models import IMUser, TokenPoll, CreateIMUser
 
 
 
-class FriendGroup(models.Model):
-    fgroup_name = models.CharField(max_length=100)
+#class FriendGroup(models.Model):
+#    fgroup_name = models.CharField(max_length=100)
+
+
+class FriendList(models.Model):
+    user_name = models.CharField(max_length=100)
+
+class Friend(models.Model):
+    user_name = models.CharField(max_length=100)
+    friend_name = models.CharField(max_length=100)
+    group_name = models.CharField(max_length=100)
+    list = models.ForeignKey(FriendList, on_delete=models.CASCADE)
+
