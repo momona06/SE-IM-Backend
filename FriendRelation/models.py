ad12d239f7b40768ff3ce42c96381e0cfc027d1f
毛爽然 <2021011784@secoder.net>
2023-04-06 15:53:12 +0800

    relation fixed N end

--- FriendRelation/models.py
+++ FriendRelation/models.py
@@ -7,3 +7,6 @@ from UserManage.models import IMUser, TokenPoll, CreateIMUser
 class FriendGroup(models.Model):
     fgroup_name = models.CharField(max_length=100)
 
+class Test(models.Model):
+    ffg = models.CharField(max_length=3)
+    rely = models.CharField(max_length=43,default="re")
