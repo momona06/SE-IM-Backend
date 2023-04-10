36a092e0aa5501eb8c3bada734caf7eee7f0aa65
毛爽然 <2021011784@secoder.net>
2023-04-10 21:58:14 +0800

    sql test 3

--- style-test.sh
+++ style-test.sh
@@ -1,2 +1,2 @@
-pycodestyle DjangoHW UserManage FriendRelation utils > pycodestyle-report.txt
-pylint DjangoHW UserManage FriendRelation utils > pylint-report.txt
+pycodestyle IM_Backend UserManage FriendRelation utils > pycodestyle-report.txt
+pylint IM_Backend UserManage FriendRelation utils > pylint-report.txt
