6ecd63d98d2aeadc2533e236bbceff3fa3181c32
毛爽然 <2021011784@secoder.net>
2023-04-10 23:55:06 +0800

    postgresql installed version lts

--- FriendRelation/models.py
+++ FriendRelation/models.py
@@ -1,6 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import User
-from UserManage.models import IMUser, TokenPoll, CreateIMUser
 
 from django.contrib.postgres.fields import ArrayField
 
