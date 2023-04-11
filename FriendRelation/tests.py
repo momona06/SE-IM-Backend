92f8b6391dc6ef7c2b3762086d81f877b44f760a
Uwwal <3476365499@qq.com>
2023-04-12 02:48:05 +0800

    unhappy debug 3

--- FriendRelation/tests.py
+++ FriendRelation/tests.py
@@ -46,26 +46,26 @@ class FriendRelationTest(TestCase):
         password_1 = random.randint(100_000_000_000, 999_999_999_999)
 
         self.userRegister(username, password)
-        res_login = self.userLogin(username,password)
+        res_login = self.userLogin(username, password)
 
         token = res_login.json()["token"]
 
-        res_check = self.userCheck(username,username_1,token)
+        res_check = self.userCheck(username, username_1, token)
         self.assertEqual(res_check.json()["code"], -4)
 
-        username_1 = username+1
+        username_1 = username + 1
 
         self.userRegister(username_1, password_1)
-        res_check = self.userCheck(username,username_1,token)
+        res_check = self.userCheck(username, username_1, token)
         self.assertEqual(res_check.json()["code"], 0)
 
-        res_check = self.userCheck(username-1,username_1,token)
+        res_check = self.userCheck(username - 1, username_1, token)
         self.assertEqual(res_check.json()["code"], -3)
 
-        res_check = self.userCheck(username,username_1+1,token)
+        res_check = self.userCheck(username, username_1 + 1, token)
         self.assertEqual(res_check.json()["code"], -20)
 
-        res_check = self.userCheck(username,username_1,0)
+        res_check = self.userCheck(username, username_1, 0)
         self.assertEqual(res_check.json()["code"], -2)
 
     def testSearchUser(self):
@@ -81,16 +81,15 @@ class FriendRelationTest(TestCase):
         self.userRegister(username_2, password)
         self.userRegister(username_3, password)
 
-        #user_model = get_user_model()
+        # user_model = get_user_model()
 
-        #user = user_model.objects.filter(username=username).first()
-        #user_1 = user_model.objects.filter(username=username_1).first()
-        #user_2 = user_model.objects.filter(username=username_2).first()
-        #user_3 = user_model.objects.filter(username=username_3).first()
+        # user = user_model.objects.filter(username=username).first()
+        # user_1 = user_model.objects.filter(username=username_1).first()
+        # user_2 = user_model.objects.filter(username=username_2).first()
+        # user_3 = user_model.objects.filter(username=username_3).first()
 
         res = self.userSearch(username, username)
         self.assertJSONEqual(res.content, {"code": 0, "info": "Search Succeed"})
         self.assertEqual(res.json()["code"], 0)
 
         self.assertEqual(res.json()["search_user_list"][0], username_1)
-
