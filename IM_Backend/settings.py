8ccd436ba873be1e277f751b3d030e4713990ce8
Uwwal <3476365499@qq.com>
2023-04-12 00:06:17 +0800

    happy debug 4

--- IM_Backend/settings.py
+++ IM_Backend/settings.py
@@ -94,32 +94,6 @@ ASGI_APPLICATION = "IM_Backend.asgi.application"
 # }
 
 
-if DEBUG == False:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',  # 默认
-            'NAME': 'im',  # 连接的数据库
-            'HOST': 'https://database-postgresql.OverFlowLab.secoder.local',  # ip地址
-            'PORT': 5432,  # 端口
-            'USER': 'postgres',  # 用户名
-            'PASSWORD': '123456'  # 密码
-        }  # 连接psql
-    }
-
-
-# 本地PostGreSQL
-elif DEBUG == False:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'postgres',   # 连接的数据库
-            'HOST': 'localhost',  # 网址
-            'PORT': 5432,         # 端口
-            'USER': 'postgres',   # 用户名
-            'PASSWORD': '1234'    # 密码
-        }
-    }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
