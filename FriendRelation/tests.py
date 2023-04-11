ea2db554553035012f6a4653880f288d3db18114
聂子豪 <2021050007@secoder.net>
2023-04-11 18:59:35 +0800

    testify testimony test ASDF

--- FriendRelation/tests.py
+++ FriendRelation/tests.py
@@ -8,4 +8,24 @@ from UserManage.models import IMUser
 
 
 class FriendRelationTest(TestCase):
-    pass
+    def userCheck(self, my_username,check_name, token):
+        payload = {
+            "username": my_username,
+            "password": check_name,
+            "token": token,
+        }
+        return self.client.post("/friend/checkuser", data=payload, content_type="application/json")
+
+
+    def userSearch(self, my_username, search_username):
+        payload = {
+            "username": my_username,
+            "password": search_username,
+        }
+        return self.client.post("/friend/searchuser", data=payload, content_type="application/json")
+
+    def testCheckUser(self):
+        pass
+
+    def testSearchUser(self):
+        pass
