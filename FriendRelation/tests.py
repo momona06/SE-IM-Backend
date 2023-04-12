a3d72967b47c2626b03dbc13b3f0a211a6053451
聂子豪 <2021050007@secoder.net>
2023-04-12 20:09:33 +0800

    test 105

--- FriendRelation/tests.py
+++ FriendRelation/tests.py
@@ -72,7 +72,7 @@ class FriendRelationTest(TestCase):
         res_check = self.userCheck(username, username_1, 0)
         self.assertEqual(res_check.json()["code"], -2)
 
-    '''
+
     def testSearchUser(self):
         username = random.randint(100_000_000_000, 999_999_999_999)
         password = random.randint(100_000_000_000, 999_999_999_999)
@@ -98,4 +98,3 @@ class FriendRelationTest(TestCase):
         self.assertEqual(res.json()["code"], 0)
 
         self.assertEqual(res.json()["search_user_list"][0], username_1)
-    '''
