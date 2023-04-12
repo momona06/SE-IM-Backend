d0f30a9747e80388f46592efecf6a40aef5cd4fe
聂子豪 <2021050007@secoder.net>
2023-04-13 04:23:40 +0800

    test 150

--- UserManage/views.py
+++ UserManage/views.py
@@ -165,7 +165,7 @@ def user_register(request: HttpRequest):
                 friend_list = FriendList(user_name=username, group_list=group_list, friend_list=friend_list_tem)
                 friend_list.save()
                 new_list = FriendList.objects.get(user_name=username).friend_list
-                print("after" + new_list)
+                print("after" + str(new_list))
                 print("eeee:" + str(friend_list.friend_list))
 
                 add_list = AddList(user_name=username, reply_list=list(), reply_answer=list(), reply_ensure=list(),
