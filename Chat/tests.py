56ea4e9e4651e4ec525b92311731bbe1c585c931
聂子豪 <2021050007@secoder.net>
2023-05-11 14:57:42 +0800

    test 25

--- Chat/tests.py
+++ Chat/tests.py
@@ -52,33 +52,12 @@ class MyConsumerTestCase(TestCase):
         # 连接 WebSocket
         connected, _ = await communicator_0.connect()
         assert connected
-        connected, _ = await communicator_1.connect()
-        assert connected
 
         # 发送消息到 Consumer
         await communicator_0.send_json_to({
             "function": "add_channel",
             "username": USERNAME_0
         })
-
-        await communicator_1.send_json_to({
-            "function": "add_channel",
-            "username": USERNAME_1
-        })
-
-        await communicator_0.send_json_to({
-            "function": "heartbeat",
-        })
-
-        response = await communicator_0.receive_from()
-        assert json.loads(response)["cur_user"] == USERNAME_0
-
-        await communicator_1.send_json_to({
-            "function": "heartbeat",
-        })
-
-        response = await communicator_1.receive_from()
-        assert json.loads(response)["cur_user"] == USERNAME_1
 
         await communicator_0.send_json_to({
             "function": "heartbeat",
@@ -92,12 +71,20 @@ class MyConsumerTestCase(TestCase):
             'to': USERNAME_1,
             'from': USERNAME_0
         })
-
         await communicator_0.disconnect()
-        await communicator_1.disconnect()
 
-        await communicator_1.connect()
+        connected, _ = await communicator_1.connect()
         assert connected
+        await communicator_1.send_json_to({
+            "function": "add_channel",
+            "username": USERNAME_1
+        })
+
+        await communicator_1.send_json_to({
+            "function": "heartbeat",
+        })
+        response = await communicator_1.receive_from()
+        assert json.loads(response)["cur_user"] == USERNAME_1
 
         await communicator_1.send_json_to({
             "function": "confirm",
@@ -108,6 +95,8 @@ class MyConsumerTestCase(TestCase):
 
         response = await communicator_1.receive_from()
         assert response.json()["function"] == 'friendlist'
+
+        await communicator_1.disconnect()
 
         """
 
