005789ee3706f31dc55d49336c1f74c6de0a36b6
周晋 <2021010743@secoder.net>
2023-05-13 20:16:46 +0800

    image26

--- UserManage/models.py
+++ UserManage/models.py
@@ -11,7 +11,7 @@ def create_im_user(user, token):
 class IMUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=100)
-    avatar = models.ImageField(upload_to='')
+    avatar = models.ImageField(upload_to='pic/')
 
 
 class TokenPoll(models.Model):
