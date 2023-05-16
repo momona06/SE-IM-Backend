3cad91149cccf128c571c8bde53e61e3413745bc
聂子豪 <2021050007@secoder.net>
2023-05-17 02:20:01 +0800

    base64

--- utils/utils_cryptogram.py
+++ utils/utils_cryptogram.py
@@ -1,1 +1,15 @@
-from django.contrib.auth.hashers import make_password, check_password
+import base64
+
+# 编码
+def encode(string):
+    return base64.b64encode(string.encode())
+
+# 解码
+def decode(string):
+    return base64.b64decode(string.encode())
+
+async def async_encode(string):
+    return base64.b64encode(string.encode())
+
+async def async_decode(string):
+    return base64.b64decode(string.encode())
