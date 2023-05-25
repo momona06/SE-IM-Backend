90c3f7aed8d781fbbfd8378134770e965707a091
毛爽然 <2021011784@secoder.net>
2023-05-25 16:41:27 +0800

    deleteavatar3

--- Chat/tests.py
+++ Chat/tests.py
@@ -1,12 +1,7 @@
-from asgiref.sync import sync_to_async
-from django.contrib.auth.models import User
-from asyncio import *
 from Chat.consumers import UserConsumer
 from django.test import TestCase
-
 from channels.testing import WebsocketCommunicator
 
-from FriendRelation.models import FriendList, AddList
 
 USERNAME_0 = "test00"
 PASSWORD_0 = "123456"
@@ -15,83 +10,8 @@ PASSWORD_1 = "123456"
 
 
 class UserConsumerTest(TestCase):
-    def user_register(self, username, password):
-        payload = {
-            "username": username,
-            "password": password
-        }
-        return self.client.post("/user/register", data=payload, content_type="application/json")
-
-    def user_login(self, username, password, email=""):
-        payload = {
-            "username": username,
-            "password": password,
-            "email": email
-        }
-        return self.client.post("/user/login", data=payload, content_type="application/json")
-
-    def user_check(self, my_username, check_name, token):
-        payload = {
-            "my_username": my_username,
-            "check_name": check_name,
-            "token": token,
-        }
-        return self.client.post("/friend/checkuser", data=payload, content_type="application/json")
-
-    def user_search(self, my_username, search_username):
-        payload = {
-            "my_username": my_username,
-            "search_username": search_username,
-        }
-        return self.client.post("/friend/searchuser", data=payload, content_type="application/json")
-
-    def user_logout(self, username, token):
-        payload = {
-            "username": username,
-            "token": token
-        }
-        return self.client.delete("/user/logout", data=payload, content_type="application/json")
-
-    def user_cancel(self, username, input_password):
-        payload = {
-            "username": username,
-            "input_password": input_password
-        }
-        return self.client.delete("/user/cancel", data=payload, content_type="application/json")
-
-    # async def register(self, username, password):
-    #     tem = await sync_to_async(User.objects.create_user)(username=username, password=password)
-    #     await sync_to_async(tem.save)()
-    #
-    #     group = ['我的好友']
-    #     friend_list = await sync_to_async(FriendList)(user_name=username, group_list=group, friend_list=list())
-    #     await sync_to_async(friend_list.save)()
-    #
-    #     add_list = await sync_to_async(AddList)(user_name=username,
-    #                                             reply_list=list(), reply_answer=list(), reply_ensure=list(),
-    #                                             apply_list=list(), apply_answer=list(), apply_ensure=list())
-    #     await sync_to_async(add_list.save)()
-    #
-    # async def test_heartbeat(self):
-    #     communicator_0 = WebsocketCommunicator(UserConsumer.as_asgi(), "/wsconnect")
-    #
-    #     # 连接 WebSocket
-    #     connected, _ = await communicator_0.connect()
-    #     assert connected
-    #
-    #     await communicator_0.send_json_to({
-    #         "function": "heartbeat",
-    #     })
-    #
-    #     response = await communicator_0.receive_json_from()
-    #     self.assertEqual(response['function'], 'heartbeatconfirm')
 
     async def test_add_channel(self):
-        # res_reg = await sync_to_async(self.user_register)(USERNAME_0, PASSWORD_0)
-        # res_lin = await sync_to_async(self.user_login)(USERNAME_0, PASSWORD_0)
-        # self.assertEqual(res_reg.json()["code"], 0)
-        # self.assertEqual(res_lin.json()["code"], 0)
-
         communicator = WebsocketCommunicator(UserConsumer.as_asgi(), "/wsconnect")
 
         connected, _ = await communicator.connect()
